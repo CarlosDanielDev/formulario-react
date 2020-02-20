f4a8cdb0bbb329d26be47c345cb407e008da8264
DanPHP7 <killer.dk20@gmail.com>
2020-02-20 10:27:29 -0300

    relaçoes com formulario

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { Form } from "@unform/web";
 import Input from "./components/Form/Input.js";
 import "./App.css";
 
+const initialData = {
+  name: "Carlos Daniel",
+  email: "daniel@suldopara.com.br",
+  pass: "123abcd"
+};
+
 function App() {
   function handleSubmit(data) {
     console.log(data);
@@ -10,11 +16,13 @@ function App() {
   return (
     <div className="App">
       <h1>Form</h1>
-      <Form onSubmit={handleSubmit}>
-        <Input name="name" />
-        <Input type="email" name="email" />
-        <Input type="password" name="pass" />
-
+      <Form initialData={initialData} onSubmit={handleSubmit}>
+        <Input name="user.name" /> <br />
+        <Input type="email" name="user.email" /> <br />
+        <Input type="password" name="user.pass" /> <br />
+        <Input name="address.street" /> <br />
+        <Input name="address.number" /> <br />
+        <Input name="address.city" /> <br />
         <button type="submit">Submit</button>
       </Form>
     </div>
--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -13,5 +13,5 @@ export default function Input({ name, ...rest }) {
     });
   }, [fieldName, registerField]);
 
-  return <input ref={inputRef} {...rest} />;
+  return <input ref={inputRef} defaultValue={defaultValue} {...rest} />;
 }
